7ab33a707ee8915ba3d60bcb78aef7b5948fa6ec
illusionist99 <luissram55@gmail.com>
2024-02-25 17:28:58 -0500

    final version

--- part3/setup.sh
+++ part3/setup.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
 # create cluster
-k3d cluster create mycluster  --port 8080:80@loadbalancer --port 8888:8888@loadbalancer
+k3d cluster create mycluster --port 8888:8888@loadbalancer
 
 # install argoCD
 kubectl create namespace argocd
 kubectl apply -n argocd -f install.yaml
 
 # waiting for pods to be ready
-
 while [ "$(kubectl get pods -n argocd -o jsonpath='{.items[*].status.containerStatuses[0].ready}')" != "true true true true true true true" ]; do
    sleep 5
    echo "\rWaiting for argoCD Pods to be ready..."
 done
 
-#kubectl wait -f install.yaml
-#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 # forwarding service to localhost 443
-#kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 echo "Forwarded ArgoCD [+]"
 
 
@@ -28,9 +25,11 @@ kubectl -n argocd patch secret argocd-secret -p '{"stringData":  {
 }}'
 
 #-----------------DEPLOY--APP-------------------#
+# create namespace DEV
 
 kubectl create namespace dev
 
+# deploy app from config
 kubectl apply -f project.yaml -n argocd 
 kubectl apply -f application.yaml -n argocd
 
